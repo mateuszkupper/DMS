8b114b1ae0e71b85bdba9cba64b90c32973662ba
Mateusz Kupper <mkupper@agencyport.com>
2017-06-14 11:59:06 -0400

    MySQLManager implemented

--- src/database/MySQLManager.java
+++ src/database/MySQLManager.java
@@ -1,29 +1,56 @@
 package database;
+import org.hibernate.Session;  
+import org.hibernate.SessionFactory;  
+import org.hibernate.Transaction;  
+import org.hibernate.cfg.Configuration;  
 
 public class MySQLManager implements PhysicalDBImplementation {
-
+	private static Configuration hibernateConfig;
+	
+	static {
+	    hibernateConfig = new Configuration();  
+	    hibernateConfig.configure("hibernate.cfg.xml"); 
+	}
+	
 	@Override
 	public Object retrieve(Class<?> c, String attribute, String value) {
-		// TODO Auto-generated method stub
-		return null;
+	    Session session = getHibernateSession(); 
+	    Transaction transaction = session.beginTransaction();  			
+        Object obj = (Object)session.get(c, value);     
+	    transaction.commit(); 
+	    session.close();	
+		return obj;
 	}
 
 	@Override
 	public void persist(Object object) {
-		// TODO Auto-generated method stub
-		
+	    Session session = getHibernateSession(); 
+	    Transaction transaction = session.beginTransaction();        
+	    session.persist(object);        
+	    transaction.commit(); 
+	    session.close();  		
 	}
 
 	@Override
 	public void update(Object object) {
-		// TODO Auto-generated method stub
-		
+	    Session session = getHibernateSession(); 
+	    Transaction transaction = session.beginTransaction();  			
+		session.update(object);        
+	    transaction.commit(); 
+	    session.close();		
 	}
 
 	@Override
 	public void delete(Object object) {
-		// TODO Auto-generated method stub
-		
+	    Session session = getHibernateSession(); 
+	    Transaction transaction = session.beginTransaction();  			
+		session.delete(object);        
+	    transaction.commit(); 
+	    session.close();
 	}
-
+	
+	private Session getHibernateSession() {
+	    SessionFactory factory = hibernateConfig.buildSessionFactory();  
+	    return factory.openSession();     		
+	}
 }
